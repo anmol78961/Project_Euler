6f76a7686c7e5c67e83d9a5243bc1ad2ca281bcc
Anmol Madaan <56681449+anmol78961@users.noreply.github.com>
2021-10-23 13:14:08 +0400

    Create Multiples3or5.java

---
+++ Multiples3or5.java
@@ -0,0 +1,14 @@
+class Multiples3or5 {
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+      if (i % 3 == 0 && i % 5 == 0)
+        sum += i;
+      else if (i % 3 == 0 || i % 5 == 0)
+        sum += i;
+      else
+        continue;
+    }
+    System.out.println(sum);
+  }
+}
